59238edf50c223aa3643d92fa16236c45a17a763
Little Trash <70831884+twentysevns@users.noreply.github.com>
2024-01-08 16:33:43 +0700

    Update 1.clone.sh

--- preset-lede/1.clone.sh
+++ preset-lede/1.clone.sh
@@ -10,6 +10,5 @@ rm -rf package/lean/luci-theme-argon
 mkdir -p package/_supply_packages && cd package/_supply_packages
 git clone --depth 1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git
-git clone --depth 1 https://github.com/kenzok8/openwrt-packages.git
-git clone --depth 1 https://github.com/kenzok8/small.git
-git clone --depth 1 https://github.com/Ausaci/luci-app-nat6-helper.git
+
+
